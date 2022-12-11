c222f4ef2ef1add003bc7d9ccc3ff33aa8690110
ADITYA SHARMA <84766632+AdityaSharma224@users.noreply.github.com>
2022-12-11 17:39:53 +0530

    Add files via upload

---
+++ Server.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const routes = require("./routes/ToDoRoute");
+const cors = require('cors')
+
+require("dotenv").config();
+const app = express();
+//mongoose.set(" strictQuery", true);
+const port = process.env.port || 5000;
+
+app.use(express.json())
+app.use(cors())
+
+mongoose
+  .connect(process.env.MONGODB_URL)
+  .then(() => console.log(`Connected To MongoDB...`))
+  .catch((err) => {
+    console.log("error");
+  });
+
+app.use(routes);
+
+app.listen(port, () => console.log(`Listening on: ${port}`));
