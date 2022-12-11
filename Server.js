2b90b21deb88dcd0ad0513afacb912ea70dde558
ADITYA SHARMA <84766632+AdityaSharma224@users.noreply.github.com>
2022-12-11 18:02:31 +0530

    Add files via upload

--- Server.js
+++ Server.js
@@ -5,7 +5,7 @@ const cors = require('cors')
 
 require("dotenv").config();
 const app = express();
-//mongoose.set(" strictQuery", true);
+//mongoose.set(" strictQuery", false);
 const port = process.env.port || 5000;
 
 app.use(express.json())
